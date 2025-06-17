f3e66dde176aedc52321649f5a96994ff7571009
Eric-Philippe <ericphlpp@gmail.com>
2025-06-17 10:11:24 +0200

    feat: initialize public API gateway with TypeScript, Express, and proxy setup
    - Added package.json with necessary dependencies and scripts for building and running the server.
    - Implemented proxy middleware in proxy.ts to handle routing based on defined routes.
    - Defined routes in routes.ts with configurations for authentication, credit checks, and proxy targets.
    - Set up the main server in server.ts to initialize the Express application and configure error handling.
    - Configured TypeScript settings in tsconfig.json for the project.

---
+++ api-gateway/src/routes.ts
@@ -0,0 +1,39 @@
+interface Route {
+    url: string;
+    auth: boolean;
+    creditCheck: boolean;
+    rateLimit?: {
+        windowMs: number;
+        max: number;
+    };
+    proxy: {
+        target: string;
+        router?: { [key: string]: string };
+        changeOrigin: boolean;
+        pathFilter?: string;
+        pathRewrite?: { [key: string]: string };
+    };
+}
+
+const ROUTES: Route[] = [
+    {
+        url: '/public',
+        auth: false,
+        creditCheck: false,
+        proxy: {
+            target: "http://localhost:5050",
+            changeOrigin: true,
+        }
+    },
+    {
+        url: '/private',
+        auth: false,
+        creditCheck: false,
+        proxy: {
+            target: "http://localhost:5555",
+            changeOrigin: true,
+        }
+    }
+];
+
+export { ROUTES, Route };
